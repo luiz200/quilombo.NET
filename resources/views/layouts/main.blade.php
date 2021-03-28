7203af7c5a68402c4efccf589825fbc9f4145a68
luiz200 <luiz.fhs2015@gmail.com>
2021-03-28 14:00:04 -0300

    Responsividade instalada

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,112 +7,72 @@
     
     <link rel="stylesheet" href="/css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
 
     <title>@yield('title')</title>
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-    <style type="text/css">
-    /* Chart.js */
-    @keyframes chartjs-render-animation{from{opacity:.99}to{opacity:1}}.chartjs-render-monitor{animation:chartjs-render-animation 1ms}.chartjs-size-monitor,.chartjs-size-monitor-expand,.chartjs-size-monitor-shrink{position:absolute;direction:ltr;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1}.chartjs-size-monitor-expand>div{position:absolute;width:1000000px;height:1000000px;left:0;top:0}.chartjs-size-monitor-shrink>div{position:absolute;width:200%;height:200%;left:0;top:0}
-    </style>
-    
 </head>
 <body >
-    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap" id="nav">
-        <a class="navbar-brand col-md-5 col-lg-2 me-0 px-5" id="dd" href="/">Quilombo</a>
-        <button class="navbar-toggler position-absolute d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="true" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <form class="w-100" action="/" method="get">
-            <input id="search" name="search" class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-        </form>
-        <ul class="navbar-nav px-5">
+    <header>
+      <nav>
+        <input type="checkbox" id="check">
+		<label for="check" class="checkbtn">
+			<i class="fas fa-bars"><ion-icon name="menu-outline"></ion-icon></i>
+		</label>
+        <label class="logo">Quilombo.Net</label>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
             @guest
-            <li class="nav-item text-nowrap">
-                <a class="nav-link" href="/login" id="ddd"><ion-icon name="enter-outline"></ion-icon></a>
+            <li>
+                <a href="/register">Cadastrar Quilombo</a>
             </li>
             @endguest
             @auth
-            <li class="nav-item text-nowrap">
+            <li>
+                <a href="/users/publiction">Publicar</a>
+            </li>
+            <li>
+                <a href="/dashboard">Perfil</a>
+            </li>
+            @endauth
+            @guest
+            <li>
+                <a href="/users/contact">Contatos</a>
+            </li>
+            @endguest    
+            @guest
+            <li>
+                <a href="/login" >Entrar</a>
+            </li>
+            @endguest
+            @auth
+            <li>
                 <form action="/logout" method="POST">
                     @csrf
-                    <a class="nav-link" href="/logout" 
+                    <a href="/logout" 
                         onclick="event.preventDefault();
                         this.closest('form').submit();"
                     >
-                    <ion-icon name="exit-outline" id="d"></ion-icon>
+                    Sair
                     </a>
                 </form>
             </li>
             @endauth
         </ul>
+      </nav>
     </header>
-    <div class="container-fluid">
-        <div class="row">
-            <nav id="sidebarMenu" class="col-md-5 col-lg-2 d-md-block bg-light bg-gradient sidebar collapse row">
-                <div class="position-sticky pt-4">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/"><ion-icon name="home-outline"></ion-icon> Home</a>
-                        </li>
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="/register"><ion-icon name="add-outline"></ion-icon> Cadastrar Quilombo</a>
-                        </li>
-                        @endguest
-                        @auth
-                        <li class="nav-item">
-                            <a class="nav-link" href="/users/publiction"><ion-icon name="add-circle-outline"></ion-icon> Publicar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/dashboard"><ion-icon name="person-circle-outline"></ion-icon> Perfil</a>
-                        </li>
-                        @endauth
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="/users/contact"><ion-icon name="call-outline"></ion-icon> Contatos</a>
-                        </li>
-                        @endguest    
-                    </ul>    
-                </div>
-            </nav>
-        </div>
-    </div>
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="chartjs-size-monitor">
-                    <div class="chartjs-size-monitor-expand">
-                        <div class>
-                        </div>
-                    </div>
-                    <div class="chart-size-monitor-shrink">
-                        <div class>
-                        </div>
-                    </div>
-                </div>
-                @if(session('msg'))
-                    <p class="msg">{{ session('msg') }}</p>
-                @endif
-                @yield('content')
-            </div>    
-        </div>
-    </main>
+    <section>
+        @if(session('msg'))
+            <p class="msg">{{ session('msg') }}</p>
+        @endif
+        <br>
+        <br>
+        <form action="/" method="get">
+            <input id="search-container" class="form-control form-control border border-warnings" name="search" type="text" placeholder="Search" aria-label="Search">
+        </form>
+        @yield('content')
+    </section>
 </body>
 <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
 </html>
